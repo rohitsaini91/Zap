c505c3209a61b2425b3f933c00e9c018a6c9029b
Rohit Saini <sketchlearn7@gmail.com>
2020-01-14 16:00:01 +0530

    added

---
+++ FormApp/Controller/NewFormVC.swift
@@ -0,0 +1,41 @@
+//
+//  NewFormVC.swift
+//  FormApp
+//
+//  Created by Rohit Saini on 13/01/20.
+//  Copyright © 2020 Rohit Saini. All rights reserved.
+//
+
+import UIKit
+import AnimatedTextInput
+
+class NewFormVC: UIViewController {
+    
+    
+    @IBOutlet weak var FormTitle: AnimatedTextInput!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        // Do any additional setup after loading the view.
+    }
+    
+   //MARK:- configUI
+    private func configUI(){
+        FormTitle.placeHolderText = "Form Title"
+        FormTitle.type = .multiline
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '12.0'
+
+target 'FormApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FormApp
+  pod 'AnimatedTextInput'
+
+  target 'FormAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FormAppUITests' do
+    # Pods for testing
+  end
+
+end
